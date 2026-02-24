7d4eb2eb2687fc98e2da6c50b629c00edbcc979c
giosue74 <toxicgiosue@gmail.com>
2026-02-24 16:24:57 +0100

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,16 +8,13 @@ int main(void)
 {
 	int combo;
 
-	for (combo =0; combo <= 9; combo++)
+	for (combo = 0; combo <= 9; combo++)
 		{
 			if (combo != 0)
-			{
 				putchar(' ');
 				putchar('0' + combo);
-			
 			if (combo != 9)
 				putchar(',');
-			}
 		}
 	putchar('\n');
 	return (0);
