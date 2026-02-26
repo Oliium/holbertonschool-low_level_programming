4d5ea3e0dc3415b776a9ff39a200ad427768fb86
olium <jeremie.rouxel@outlook.fr>
2026-02-26 20:06:02 +0100

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -11,7 +11,7 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-		_putchar (\n);
+		_putchar ('\n');
 		return;
 	}
 
@@ -20,5 +20,5 @@ void print_line(int n)
 		_putchar ('_');
 	}
 
-	_putchar ('\n')
+	_putchar ('\n');
 }
