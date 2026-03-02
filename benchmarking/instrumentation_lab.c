0825b3d262d1881da15fb61c9faf50dd87bbcabc
Olium <jeremie.rouxel@outlook.fr>
2026-03-02 14:48:45 +0100

    instrumentation_lab.c

--- benchmarking/instrumentation_lab.c
+++ benchmarking/instrumentation_lab.c
@@ -8,92 +8,84 @@ static int dataset[DATASET_SIZE];
 
 static unsigned int next_value(unsigned int *state)
 {
-	*state = (*state * 1103515245u) + 12345u;
-	return *state;
+    *state = (*state * 1103515245u) + 12345u;
+    return *state;
 }
 
 static void build_dataset(void)
 {
-	unsigned int state;
-	int i;
+    unsigned int state = SEED_VALUE;
+    int i;
 
-	state = SEED_VALUE;
-
-	for (i = 0; i < DATASET_SIZE; i++)
-		dataset[i] = (int)(next_value(&state) % 100000);
+    for (i = 0; i < DATASET_SIZE; i++)
+        dataset[i] = (int)(next_value(&state) % 100000);
 }
 
 static void process_dataset(void)
 {
-	int i;
-	int v;
+    int i;
+    int v;
 
-	for (i = 0; i < DATASET_SIZE; i++)
-	{
-		v = dataset[i];
-		v = (v * 3) + (v / 7) - (v % 11);
-		if (v < 0)
-			v = -v;
-		dataset[i] = v;
-	}
+    for (i = 0; i < DATASET_SIZE; i++)
+    {
+        v = dataset[i];
+        v = (v * 3) + (v / 7) - (v % 11);
+        if (v < 0)
+            v = -v;
+        dataset[i] = v;
+    }
 }
 
 static unsigned long reduce_checksum(void)
 {
-	unsigned long sum;
-	int i;
+    unsigned long sum = 0;
+    int i;
 
-	sum = 0;
-	for (i = 0; i < DATASET_SIZE; i++)
-		sum = (sum * 131ul) + (unsigned long)dataset[i];
+    for (i = 0; i < DATASET_SIZE; i++)
+        sum = (sum * 131ul) + (unsigned long)dataset[i];
 
-	return sum;
+    return sum;
 }
 
 int main(void)
 {
-	unsigned long checksum;
+    unsigned long checksum;
+    clock_t total_start;
+    clock_t total_end;
+    clock_t start;
+    clock_t end;
+    double total_time;
+    double build_time;
+    double process_time;
+    double reduce_time;
 
-	clock_t total_start, total_end;
-	clock_t start, end;
+    total_start = clock();
 
-	double total_time;
-	double build_time;
-	double process_time;
-	double reduce_time;
+    start = clock();
+    build_dataset();
+    end = clock();
+    build_time = (double)(end - start) / (double)CLOCKS_PER_SEC;
 
-	/* TOTAL start */
-	total_start = clock();
+    start = clock();
+    process_dataset();
+    end = clock();
+    process_time = (double)(end - start) / (double)CLOCKS_PER_SEC;
 
-	/* BUILD phase */
-	start = clock();
-	build_dataset();
-	end = clock();
-	build_time = (double)(end - start) / CLOCKS_PER_SEC;
+    start = clock();
+    checksum = reduce_checksum();
+    end = clock();
+    reduce_time = (double)(end - start) / (double)CLOCKS_PER_SEC;
 
-	/* PROCESS phase */
-	start = clock();
-	process_dataset();
-	end = clock();
-	process_time = (double)(end - start) / CLOCKS_PER_SEC;
+    total_end = clock();
+    total_time = (double)(total_end - total_start) / (double)CLOCKS_PER_SEC;
 
-	/* REDUCE phase */
-	start = clock();
-	checksum = reduce_checksum();
-	end = clock();
-	reduce_time = (double)(end - start) / CLOCKS_PER_SEC;
+    if (checksum == 0ul)
+        return 1;
 
-	/* TOTAL end */
-	total_end = clock();
-	total_time = (double)(total_end - total_start) / CLOCKS_PER_SEC;
+    printf("TOTAL seconds: %.6f\n", total_time);
+    printf("BUILD_DATA seconds: %.6f\n", build_time);
+    printf("PROCESS seconds: %.6f\n", process_time);
+    printf("REDUCE seconds: %.6f\n", reduce_time);
 
-	if (checksum == 0ul)
-		return 1;
-
-	printf("TOTAL seconds: %.6f\n", total_time);
-	printf("BUILD_DATA seconds: %.6f\n", build_time);
-	printf("PROCESS seconds: %.6f\n", process_time);
-	printf("REDUCE seconds: %.6f\n", reduce_time);
-
-	return 0;
+    return 0;
 }
