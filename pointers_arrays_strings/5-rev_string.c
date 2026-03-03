beaedc44d6b4ddd889031f4043edc2984917daff
olium <jeremie.rouxel@outlook.fr>
2026-03-03 17:04:13 +0100

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: pointer to the string to be reversed
+ */
+
+void rev_string(char *s)
+{
+    int length = 0;
+    char *start = s;
+    char temp;
+
+    while (*s != '\0')
+    {
+        length++;
+        s++;
+    }
+
+    s--; /* Move back to the last character of the string */
+
+    while (start < s)
+    {
+        temp = *start;
+        *start = *s;
+        *s = temp;
+
+        start++;
+        s--;
+    }
+}
