5b3d94ee10977dff34b17792f5a2cac3e323babc
olium <jeremie.rouxel@outlook.fr>
2026-03-03 16:48:04 +0100

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line.
+ * @s: pointer to the string to be printed in reverse
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	s--; /* Move back to the last character of the string */
+
+	while (length > 0)
+	{
+		_putchar(*s);
+		s--;
+		length--;
+	}
+	_putchar('\n');
+}
