25780f9cef70b6d84ead585fe5c5622e5b409a5c
olium <jeremie.rouxel@outlook.fr>
2026-03-04 16:24:32 +0100

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strcat - concatenates two strings.
+ * _strcat - concatenates two strings.
  * @dest: pointer to the destination string
  * @src: pointer to the source string
  * Return: pointer to the resulting string dest
