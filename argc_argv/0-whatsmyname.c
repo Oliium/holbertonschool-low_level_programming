c63188d23c358c979911633f4b745746a02df608
olium <jeremie.rouxel@outlook.fr>
2026-03-06 17:35:43 +0100

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+
 int main(int argc, char *argv[])
 {
 	(void)argc;
