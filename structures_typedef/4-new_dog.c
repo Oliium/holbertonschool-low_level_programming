0283e3ec53eef1fd9ed9548f348fbb6ecf026277
olium <jeremie.rouxel@outlook.fr>
2026-03-15 23:37:55 +0100

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,39 +9,26 @@
  *
  * Return: pointer to the new dog, or NULL if it fails
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
 	d = malloc(sizeof(dog_t));
 
 	if (d == NULL)
-
 		return (NULL);
 
-	d->name = malloc(strlen(name) + 1);
+	d->name = _strdup(name);
+	d->owner = _strdup(owner);
+	d->age = age;
 
-	if (d->name == NULL)
-	{
-		free(d);
-		return (NULL);
-	}
-
-	d->owner = malloc(strlen(owner) + 1);
-
-	if (d->owner == NULL)
+	if (d->name == NULL || d->owner == NULL)
 	{
 		free(d->name);
+		free(d->owner);
 		free(d);
 		return (NULL);
 	}
 
-	strcpy(d->name, name);
-	d->age = age;
-	strcpy(d->owner, owner);
 	return (d);
 }
