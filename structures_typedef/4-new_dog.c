301ab4069213bfc8a5acdd50b5f8995825d36aab
olium <jeremie.rouxel@outlook.fr>
2026-03-15 23:38:58 +0100

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - creates a new dog
@@ -14,21 +15,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 
 	d = malloc(sizeof(dog_t));
-
 	if (d == NULL)
 		return (NULL);
 
-	d->name = _strdup(name);
-	d->owner = _strdup(owner);
-	d->age = age;
-
-	if (d->name == NULL || d->owner == NULL)
+	d->name = malloc(strlen(name) + 1);
+	if (d->name == NULL)
 	{
-		free(d->name);
-		free(d->owner);
 		free(d);
 		return (NULL);
 	}
+
+	d->owner = malloc(strlen(owner) + 1);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
+	strcpy(d->name, name);
+	strcpy(d->owner, owner);
+	d->age = age;
 
 	return (d);
 }
