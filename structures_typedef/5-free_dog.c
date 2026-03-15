40a8b7622fd4c1dc1341d4de5ffba09ff9e2dc68
olium <jeremie.rouxel@outlook.fr>
2026-03-15 23:45:58 +0100

    5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * free_dog - frees a dog
+ * @d: dog to free
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
