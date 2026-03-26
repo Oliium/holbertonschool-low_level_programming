bae84551930bd421194b012485c7aee48214e6e7
Olium <jeremie.rouxel@outlook.fr>
2026-03-26 14:56:09 +0100

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,16 +1,16 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: pointer to the list_t list to print
- * Return: the number of nodes in the list
+ * print_list -prints strings contained in different nodes
+ * @h: points towards the list
+ * Return: length (Length of the strings)
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t length = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 		{
@@ -21,7 +21,7 @@ size_t print_list(const list_t *h)
 			printf("[%u] %s\n", h->len, h->str);
 		}
 		h = h->next;
-		count++;
+		length++;
 	}
-	return (count);
+	return (length);
 }
