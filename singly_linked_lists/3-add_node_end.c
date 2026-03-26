f9a02161337de4899e42a068c98ba0a3ee9620d4
Olium <jeremie.rouxel@outlook.fr>
2026-03-26 15:09:13 +0100

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,49 +1,55 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * list_t *add_node_end - Function that adds a node at the end of the list
- * @head: Pointer to the pointer to the first node of the list
- * @str: Pointer that stores the string
- * Return: new node (Returns the new node)
+ * add_node_end - Function that places a new node at the end
+ * of the list
+ * @head: Points to the pointer to the first node
+ * @str: String stored
+ * Return: new_node_end (Added a new node at the end of the list)
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *current;
-	unsigned int len = 0;
-	if (head == NULL || str == NULL)
+	list_t *new_node_end;
+	list_t *index_node = NULL;
+	unsigned int length = 0;
+
+	if (head == 0 || str == 0)
 	{
 		return (NULL);
 	}
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	new_node_end = malloc(sizeof(list_t));
+	if (new_node_end == 0)
 	{
 		return (NULL);
 	}
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	new_node_end->str = strdup(str);
+	if (new_node_end->str == 0)
 	{
-		free(new_node);
+		free(new_node_end);
 		return (NULL);
 	}
-	while (str[len] != '\0')
+	while (str[length] != '\0')
 	{
-		len++;
+		length++;
 	}
-	new_node->len = len;
-	new_node->next = NULL;
+	new_node_end->len = length;
+	new_node_end->next = NULL;
 	if (*head == NULL)
 	{
-		*head = new_node;
+		*head = new_node_end;
+		return (new_node_end);
 	}
 	else
 	{
-		current = *head;
-		while (current->next != NULL)
+		index_node = *head;
+		while (index_node->next != NULL)
 		{
-			current = current->next;
+			index_node = index_node->next;
 		}
-		current->next = new_node;
+		index_node->next = new_node_end;
 	}
-	return (new_node);
+	return (new_node_end);
 }
