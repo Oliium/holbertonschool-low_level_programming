d5c8a265ccb63c28222562a04e9d636550ee0a69
Olium <jeremie.rouxel@outlook.fr>
2026-03-30 09:43:36 +0200

    .

--- doubly_linked_lists/main.h
+++ doubly_linked_lists/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -18,7 +20,7 @@ typedef struct dlistint_s
 } 
 dlistint_t;
 
-#include <stddef.h>
+
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
@@ -29,6 +31,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-
 
 #endif
